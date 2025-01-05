ddbdbfb2ff6792e1a4afa2be2ae4ddb32f4d5574
Chessy <ilovepakpak@gmail.com>
2025-01-06 04:01:50 +0700

    Refactor Folder Structure , Change Security Config , Crsf Disable

--- identityservice/src/main/java/com/example/IdentityService/securityconfig/Configuration.java
+++ identityservice/src/main/java/com/example/IdentityService/securityconfig/Configuration.java
@@ -1,4 +1,4 @@
-package com.example.IdentityService.configuration;
+package com.example.IdentityService.securityconfig;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -29,6 +30,7 @@ public class Configuration {
                         response.sendRedirect("/oauth2/userInfo/google");
                     }
                 })).formLogin(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
                 .build();
     }
 }
