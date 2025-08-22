86cea1cb9994d57471a1affc162887eaec2b4c89
Chessy <ilovepakpak@gmail.com>
2025-08-22 20:47:58 +0700

    feat(web): update homepage UI and add new asset images
    - Updated homepage components (HTML, SCSS, TS)
    - Modified header and footer styles
    - Updated login page SCSS
    - Added new images to assets: black-second-poster, white-second-poster, ndbanner, stbanner
    - Updated global styles.scss

--- webapp/psyconnect-web/src/app/pages/homepage/homepage.ts
+++ webapp/psyconnect-web/src/app/pages/homepage/homepage.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ThemeService } from '../../services/theme/theme-service';
 
 @Component({
   selector: 'app-homepage',
@@ -8,4 +9,16 @@ import { RouterModule } from '@angular/router';
   templateUrl: './homepage.html',
   styleUrl: './homepage.scss',
 })
-export class Homepage {}
+export class Homepage {
+  constructor(private themeService: ThemeService) {}
+  bannerUrl = 'assets/images/ndbanner-light.png';
+  ngOnInit(): void {
+    if (this.themeService.getTheme() == 'light') {
+        console.log("Light banner");
+
+      this.bannerUrl = 'assets/images/white-second-poster.avif';
+    } else {
+      this.bannerUrl = 'assets/images/black-second-poster.avif';
+    }
+  }
+}
