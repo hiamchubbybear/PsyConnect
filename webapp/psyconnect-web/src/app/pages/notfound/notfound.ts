86ea1b1a7264137f6f327b8e72c47dcb43289c9d
Chessy <ilovepakpak@gmail.com>
2025-08-29 00:55:04 +0700

    feat(account): update security module and account/profile pages
    - Replaced old security page with new Security Update page (HTML, SCSS, TS)
    - Updated account-update and profile-update pages
    - Refactored app routes and main app component
    - Adjusted toast component and related service
    - Minor style updates in global styles

--- webapp/psyconnect-web/src/app/pages/notfound/notfound.ts
+++ webapp/psyconnect-web/src/app/pages/notfound/notfound.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ThemeService } from '../../services/theme/theme-service';
+import { ToastType } from '../../shared/toast/toast-type';
+import { ToastService } from '../../shared/toast/toast.service';
 
 @Component({
   selector: 'app-notfound',
@@ -10,8 +12,13 @@ import { ThemeService } from '../../services/theme/theme-service';
   styleUrl: './notfound.scss',
 })
 export class Notfound {
-  constructor(private themeService: ThemeService) {}
+  constructor(
+    private themeService: ThemeService,
+    private toastService: ToastService
+  ) {}
   toggleTheme() {
     this.themeService.toggleTheme();
+    console.log('Show toast');
+    this.toastService.show('Login success', 'Success', ToastType.Success);
   }
 }
