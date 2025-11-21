84277aa8c38af7f3c030922fdb342132ad04288d
Chessy <ilovepakpak@gmail.com>
2025-11-21 08:26:46 +0700

    chore: reindex project to apply .gitignore

--- services/webapp/psyconnect-web/src/lucide.config.ts
+++
@@ -1,6 +0,0 @@
-import { importProvidersFrom } from '@angular/core';
-import { Check, ChevronLeft, ChevronRight, Eye, LucideAngularModule, MessageSquare, User, UserPlus, UserX, X } from 'lucide-angular';
-
-export const lucideConfig = importProvidersFrom(
-  LucideAngularModule.pick({ Eye, MessageSquare, UserPlus, X, Check,UserX , ChevronRight, ChevronLeft, User})
-);
