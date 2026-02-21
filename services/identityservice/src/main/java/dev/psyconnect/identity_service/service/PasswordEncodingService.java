cb389841dddb02f1eddc6f31b363088ff2f0a0c1
Chessy <ilovepakpak@gmail.com>
2026-02-21 18:48:03 +0700

    fix(identity): remove unused and leaked signer key from PasswordEncodingService

--- services/identityservice/src/main/java/dev/psyconnect/identity_service/service/PasswordEncodingService.java
+++ services/identityservice/src/main/java/dev/psyconnect/identity_service/service/PasswordEncodingService.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class PasswordEncodingService {
-    String SIGNER_KEY = "ZW5jb2RlX3Bhc3NrZXlfYW5fbG9uX2E=";
+
 
     public static String encoder(String rawPassword) {
         return new BCryptPasswordEncoder(10).encode(rawPassword);
